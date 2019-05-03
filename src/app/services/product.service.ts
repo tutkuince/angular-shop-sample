565510321d018eb01e2cdee84d5c26ea8d2da91c
tutkuince <tutku.ince@outlook.com>
2019-05-03 17:43:29 +0300

    Logging and Handling Error with Observable

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,7 +1,8 @@
 import {Injectable} from '@angular/core';
 import {Product} from '../product/product';
-import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {Observable, throwError} from 'rxjs';
+import {catchError, tap} from 'rxjs/operators';
 
 
 @Injectable({
@@ -15,6 +16,20 @@ export class ProductService {
   path = 'http://localhost:3000/products';
 
   getProductList(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.path);
+    return this.http.get<Product[]>(this.path).pipe(
+      tap(data => console.log(JSON.stringify(data))),  // logging
+      catchError(this.handleError)  // try catch
+    );
+  }
+
+  private handleError(err: HttpErrorResponse) {
+    let errorMessage = '';
+
+    if (err.error instanceof ErrorEvent) {
+      errorMessage = 'Something went wrong ' + err.error.message;
+    } else {
+      errorMessage = 'A systematic error has occurred';
+    }
+    return throwError(errorMessage);
   }
 }
