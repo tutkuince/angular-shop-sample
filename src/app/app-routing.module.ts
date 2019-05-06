badd6514d79957bf16c32d5ba3eddc3d798193dd
tutkuince <tutku.ince@outlook.com>
2019-05-06 21:14:11 +0300

    improved design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,9 @@ import {ProductComponent} from './product/product.component';
 const routes: Routes = [
   {path: 'products', component: ProductComponent},
   {path: '', redirectTo: 'products', pathMatch: 'full'},
-  {path: 'products/category/:categoryId', component: ProductComponent}
+  {path: 'products/category/:categoryId', component: ProductComponent},
+  {path: 'product-add-classic', component: ProductComponent},
+  {path: 'product-add-reactive', component: ProductComponent}
 ];
 
 @NgModule({
