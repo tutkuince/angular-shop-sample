301eb29480e1c81dd79d85392022380bd9270784
tutkuince <tutku.ince@outlook.com>
2019-05-07 13:05:16 +0300

    product-add-classic with ProductAddClassicFormComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {ProductComponent} from './product/product.component';
+import {ProductAddClassicFormComponent} from './product/product-add-classic-form/product-add-classic-form.component';
 
 const routes: Routes = [
   {path: 'products', component: ProductComponent},
   {path: '', redirectTo: 'products', pathMatch: 'full'},
   {path: 'products/category/:categoryId', component: ProductComponent},
-  {path: 'product-add-classic', component: ProductComponent},
+  {path: 'product-add-classic', component: ProductAddClassicFormComponent},
   {path: 'product-add-reactive', component: ProductComponent}
 ];
 
