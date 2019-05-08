3ac4137fc807835e6faf8eb6c8a9526870b45511
tutkuince <tutku.ince@outlook.com>
2019-05-08 16:06:23 +0300

    ProductAddReactiveFormComponent to app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {ProductComponent} from './product/product.component';
 import {ProductAddClassicFormComponent} from './product/product-add-classic-form/product-add-classic-form.component';
+import {ProductAddReactiveFormComponent} from './product/product-add-reactive-form/product-add-reactive-form.component';
 
 const routes: Routes = [
   {path: 'products', component: ProductComponent},
   {path: '', redirectTo: 'products', pathMatch: 'full'},
   {path: 'products/category/:categoryId', component: ProductComponent},
   {path: 'product-add-classic', component: ProductAddClassicFormComponent},
-  {path: 'product-add-reactive', component: ProductComponent}
+  {path: 'product-add-reactive', component: ProductAddReactiveFormComponent}
 ];
 
 @NgModule({
