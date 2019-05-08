d832eea52f9ccafd2ca45c6a0e9cfa3a988d5e84
tutkuince <tutku.ince@outlook.com>
2019-05-08 16:19:08 +0300

    injected FormBuilder

--- src/app/product/product-add-reactive-form/product-add-reactive-form.component.ts
+++ src/app/product/product-add-reactive-form/product-add-reactive-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Product} from '../product';
 
 @Component({
   selector: 'app-product-add-reactive-form',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductAddReactiveFormComponent implements OnInit {
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
+
+  productAddForm: FormGroup;
+  product: Product = new Product();
+
+  createProductAddForm() {
+    this.productAddForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      description: ['', Validators.required],
+      imageUrl: ['', Validators.required],
+      price: ['', Validators.required],
+      categoryId: ['', Validators.required]
+    });
+  }
 
   ngOnInit() {
   }
 
+  add() {
+    if (this.productAddForm.valid) {
+      this.product = Object.assign({}, this.productAddForm.value);
+    }
+  }
 }
