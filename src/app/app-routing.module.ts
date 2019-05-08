107a4b64e4f710ec55a334f0929610d97b50f2f3
tutkuince <tutku.ince@outlook.com>
2019-05-08 21:56:19 +0300

    pats of product-add-classic, product-add-reactive added parameter namely canActivate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,14 @@ import {ProductComponent} from './product/product.component';
 import {ProductAddClassicFormComponent} from './product/product-add-classic-form/product-add-classic-form.component';
 import {ProductAddReactiveFormComponent} from './product/product-add-reactive-form/product-add-reactive-form.component';
 import {LoginComponent} from './login/login.component';
+import {LoginGuard} from './login/login.guard';
 
 const routes: Routes = [
   {path: 'products', component: ProductComponent},
   {path: '', redirectTo: 'products', pathMatch: 'full'},
   {path: 'products/category/:categoryId', component: ProductComponent},
-  {path: 'product-add-classic', component: ProductAddClassicFormComponent},
-  {path: 'product-add-reactive', component: ProductAddReactiveFormComponent},
+  {path: 'product-add-classic', component: ProductAddClassicFormComponent, canActivate: [LoginGuard]},
+  {path: 'product-add-reactive', component: ProductAddReactiveFormComponent, canActivate: [LoginGuard]},
   {path: 'login', component: LoginComponent}
 ];
 
