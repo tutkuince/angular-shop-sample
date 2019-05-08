20ced6adbc4ece434cc09c66d6bdeb35685b6ffe
tutkuince <tutku.ince@outlook.com>
2019-05-08 22:23:25 +0300

    logout process

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {AccountService} from './services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-shop-sample';
+
+  constructor(private accountService: AccountService) {
+
+  }
+
+  isLoggedIn(): boolean {
+    return this.accountService.isLoggedIn();
+  }
+
+  logOut() {
+    this.accountService.logOut();
+  }
 }
